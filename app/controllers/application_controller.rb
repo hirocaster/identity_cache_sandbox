0bdc61d3c977446541fe2eb99a043111ca426147
hirocaster <hohtsuka@gmail.com>
2015-08-09 22:58:13 +0900

    Memoized cache request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  around_filter :identity_cache_memoization
+
+  def identity_cache_memoization
+    IdentityCache.cache.with_memoization{ yield }
+  end
 end
